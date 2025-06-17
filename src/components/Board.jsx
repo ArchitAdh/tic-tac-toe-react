e114767a931d9b8a87add4e0d3639bc2da6ff06d
ArchitAdh. <architadhikary@gmail.com>
2025-06-17 20:52:47 +0545

    refactor: render board using nested map for cleaner layout

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -14,10 +14,28 @@ const Board = ({ xIsNext, squares, onPlay }) => {
     onPlay(nextSquares);
   };
 
+  //create board-row
+  const boardRows = [0, 1, 2].map((row) => (
+    <div key={row} className="board-row">
+      {[0, 1, 2].map((col) => {
+        const index = row * 3 + col;
+        return (
+          <Square
+            key={index}
+            value={squares[index]}
+            onSquareClick={() => handleClick(index)}
+          />
+        );
+      })}
+    </div>
+  ));
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
+      {boardRows}
+
+      {/* <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
@@ -31,7 +49,7 @@ const Board = ({ xIsNext, squares, onPlay }) => {
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      </div> */}
     </>
   );
 };
