0bb122e92b85d930e10967fe4e139b0d64d216b9
ArchitAdh. <architadhikary@gmail.com>
2025-06-19 16:21:16 +0545

    feat: add player name inputs and game start validation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { useState } from "react";
 import Board from "./components/Board";
 import { calculateWinner } from "./utils/gameHelpers";
 const Game = () => {
+  const [gameStarted, setGameStarted] = useState(false);
+
+  // players name
+  const [playerX, setPlayerX] = useState("");
+  const [playerO, setPlayerO] = useState("");
+
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
@@ -9,8 +15,8 @@ const Game = () => {
   const result = calculateWinner(currentSquares);
   const winner = result?.winner;
   const status = winner
-    ? `Winner: ${winner}`
-    : `Next Player: ${xIsNext ? "X" : "O"}`;
+    ? `Winner: ${winner === "X" ? playerX : playerO}`
+    : `Next Player: ${xIsNext ? playerX : playerO}`;
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
@@ -23,6 +29,15 @@ const Game = () => {
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
   }
+
+  //handle game start flag
+  const handleStartGame = () => {
+    if (playerO.trim() && playerX.trim()) {
+      setGameStarted(true);
+    } else {
+      alert("Please Enter Names for both players");
+    }
+  };
 
   // reset game
   const handleReset = () => {
@@ -50,22 +65,52 @@ const Game = () => {
 
   return (
     <div className="game-container">
-      <div className="game-main">
-        <div className="game-board">
-          <Board
-            xIsNext={xIsNext}
-            squares={currentSquares}
-            onPlay={handlePlay}
+      <div className="name-inputs">
+        <div className="playerX">
+          <label htmlFor="playerX">Player X Name</label>
+          <input
+            type="text"
+            value={playerX}
+            onChange={(e) => setPlayerX(e.target.value)}
+            placeholder="Player X"
           />
         </div>
-        <div className="game-controls">
-          <p className="status">{status}</p>
-          <button onClick={handleReset}>Restart</button>
+        <div className="playerO">
+          <label htmlFor="playerO">Player O Name</label>
+          <input
+            type="text"
+            value={playerO}
+            onChange={(e) => setPlayerO(e.target.value)}
+            placeholder="Player O"
+          />
         </div>
+
+        {!gameStarted ? (
+          <button onClick={handleStartGame}>Start Game</button>
+        ) : (
+          <div className="game-controls">
+            <p className="status">{status}</p>
+            <button onClick={handleReset}>Restart</button>
+          </div>
+        )}
       </div>
-      <div className="game-info">
-        <ol>{moves}</ol>
-      </div>
+      {gameStarted && (
+        <>
+          {" "}
+          <div className="game-main">
+            <div className="game-board">
+              <Board
+                xIsNext={xIsNext}
+                squares={currentSquares}
+                onPlay={handlePlay}
+              />
+            </div>
+          </div>
+          <div className="game-info">
+            <ol>{moves}</ol>
+          </div>
+        </>
+      )}
     </div>
   );
 };
